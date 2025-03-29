7ed9cbd2103d1924c19d73cbe048e91da5251c93
pho-veteran <thanhvinhlqt2016@gmail.com>
2025-03-29 10:56:37 +0700

    Todos | Auth Form

--- app/(auth)/register/[[...register]]/components/register-form.tsx
+++ app/(auth)/register/[[...register]]/components/register-form.tsx
@@ -1,11 +1,91 @@
+import { z } from "zod";
+import React from "react";
+import { cn } from "@/lib/utils";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
-const RegisterForm = () => {
-    
-    
+const formSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8),
+    confirmPassword: z.string().min(8),
+});
+
+type RegisterFormValues = z.infer<typeof formSchema>;
+
+interface RegisterFormProps {
+    className?: string;
+}
+
+const RegisterForm: React.FC<RegisterFormProps> = ({
+    className,
+}) => {
+    const form = useForm<RegisterFormValues>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+            confirmPassword: "",
+        },
+    });
+
+    const handleRegister = (values: RegisterFormValues) => {
+        console.log(values);
+    }
+
     return (
-        <div>
-
+        <div className={cn("flex flex-col gap-6", className)}>
+            <Card>
+                <CardHeader>
+                    <CardTitle className="text-2xl">Login</CardTitle>
+                    <CardDescription>
+                        Enter your email below to login to your account
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form>
+                        <div className="flex flex-col gap-6">
+                            <div className="grid gap-2">
+                                <Label htmlFor="email">Email</Label>
+                                <Input
+                                    id="email"
+                                    type="email"
+                                    placeholder="m@example.com"
+                                    required
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <div className="flex items-center">
+                                    <Label htmlFor="password">Password</Label>
+                                    <a
+                                        href="#"
+                                        className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                                    >
+                                        Forgot your password?
+                                    </a>
+                                </div>
+                                <Input id="password" type="password" required />
+                            </div>
+                            <Button type="submit" className="w-full">
+                                Login
+                            </Button>
+                            <Button variant="outline" className="w-full">
+                                Login with Google
+                            </Button>
+                        </div>
+                        <div className="mt-4 text-center text-sm">
+                            Don&apos;t have an account?{" "}
+                            <a href="#" className="underline underline-offset-4">
+                                Sign up
+                            </a>
+                        </div>
+                    </form>
+                </CardContent>
+            </Card>
         </div>
     );
 }
--- app/(auth)/register/[[...register]]/page.tsx
+++ app/(auth)/register/[[...register]]/page.tsx
@@ -1,7 +1,10 @@
+import RegisterForm from "./components/register-form";
+
 const RegisterPage = () => {
     return (
-        <div>
+        <div className="flex items-center justify-center h-screen flex-col">
             <h1>Register</h1>
+            <RegisterForm />
         </div>
     );
 }
